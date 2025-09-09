06f66ba488cda6e218f2fd79eebeaff060ab5876
zakski <zak.szadowski+git@gmail.com>
2025-09-09 21:38:46 +0100

    basic calcs using hockey as base

--- league/leagueprep.py
+++ league/leagueprep.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 import regex as re
 import glob
 import os
 
+import leagueconst as const
 import leaguefunc as func
 
 from pathlib import Path
@@ -14,170 +16,81 @@ resultsInterDirName = os.path.join(rootDirName, Path('../results_intermediate'))
 resultsDirName = os.path.join(rootDirName, Path('../results_league'))
 
 dirLeagueName = os.path.join(rootDirName, Path('../data/data/league/'))
-dataFootballLeagueName = os.path.join(dirLeagueName, 'football.csv')
+dataFootballLeagueName = os.path.join(dirLeagueName, 'football-leagues.csv')
+dataHockLeagueName = os.path.join(dirLeagueName, 'comp-hockey.csv')
 
-footieHeader = ['level','name_base','name_prefix','name_suffix','name_full','name_short','organiser','organiser_name','clubs','entry','stages','qualification_spots','qualification_byes_format','tier_1_byes','tier_1_byes_format','tier_1_bye_entry_format','tier_1_entry_format','tier_1_intermediate','tier_1_qualifying_third','tier_1_qualifying_second','tier_1_qualifying_first','tier_1_qualifying_preliminary','tier_2_byes','tier_2_byes_format','tier_2_bye_entry_format','tier_2_entry_format','tier_2_intermediate','tier_2_qualifying_sixth','tier_2_qualifying_fifth','tier_2_qualifying_fourth','tier_2_qualifying_third','tier_2_qualifying_second','tier_2_qualifying_first','tier_2_qualifying_preliminary','amateur_cup_byes','amateur_cup_bye_entry_format','non_association_byes','non_association_bye_entry_format','non_association_other_byes','non_association_other_bye_entry_format','invite_byes','invite_bye_entry_format','format_start','format_end','type','quality','gender','victory','draw','loss','points_tiebreaker','match_system','uneven_team_system','draw_system','replay_venue','add_replay_venue','extra_time','previous_winner','home_and_away','venue_system','venue_semis','venue_finals','video_referee','promotion_teams','promotion_system','relegation_teams','relegation_system']
-
-footieTypes = {
-    'level': 'string',
-    'name_base': 'string',
-    'name_prefix': 'string',
-    'name_suffix': 'string',
-    'name_full': 'string',
-    'name_short': 'string',
-    'organiser': 'string',
-    'organiser_name': 'string',
-    'clubs': 'Int64', # entry_format
-    'entry': 'string', # entry_format
-    'stages': 'string',
-    'qualification_spots': 'Int64',
-    'qualification_byes_format': 'string',
-    'tier_1_byes': 'Int64', # qual_tier_1
-    'tier_1_byes_format': 'string',  # qual_tier_1
-    'tier_1_bye_entry_format': 'string',  # qual_tier_1
-    'tier_1_entry_format': 'string',  # qual_tier_1
-    'tier_1_intermediate': 'Int64',  # qual_tier_1
-    'tier_1_qualifying_third': 'Int64',  # qual_tier_1
-    'tier_1_qualifying_second': 'Int64',  # qual_tier_1
-    'tier_1_qualifying_first': 'Int64',  # qual_tier_1
-    'tier_1_qualifying_preliminary': 'Int64',  # qual_tier_1
-    'tier_2_byes': 'Int64', # qual_tier_2
-    'tier_2_byes_format': 'string', # qual_tier_2
-    'tier_2_bye_entry_format': 'string', # qual_tier_2
-    'tier_2_entry_format': 'string', # qual_tier_2
-    'tier_2_intermediate': 'Int64', # qual_tier_2
-    'tier_2_qualifying_sixth': 'Int64', # qual_tier_2
-    'tier_2_qualifying_fifth': 'Int64', # qual_tier_2
-    'tier_2_qualifying_fourth': 'Int64', # qual_tier_2
-    'tier_2_qualifying_third': 'Int64', # qual_tier_2
-    'tier_2_qualifying_second': 'Int64', # qual_tier_2
-    'tier_2_qualifying_first': 'Int64', # qual_tier_2
-    'tier_2_qualifying_preliminary': 'Int64', # qual_tier_2
-    'amateur_cup_byes': 'Int64',
-    'amateur_cup_bye_entry_format': 'string',
-    'non_association_byes': 'Int64',
-    'non_association_bye_entry_format': 'string',
-    'non_association_other_byes': 'Int64',
-    'non_association_other_bye_entry_format': 'string',
-    'invite_byes': 'Int64',
-    'invite_bye_entry_format': 'string',
-    'format_start': 'Int64', # format_weight
-    'format_end': 'Int64', # format_weight
-    'type': 'string', # comp_format
-    'quality': 'string', # comp_format
-    'gender': 'string', # comp_format
-    'victory': 'Int64', # score_sys
-    'draw': 'Int64', # score_sys
-    'loss': 'Int64', # score_sys
-    'points_tiebreaker': 'string', # score_sys
-    'match_system': 'string',
-    'uneven_team_system': 'string',
-    'draw_system': 'string',
-    'replay_venue': 'string',
-    'add_replay_venue': 'string',
-    'extra_time': 'string',
-    'previous_winner': 'string',
-    'home_and_away': 'bool',
-    'venue_system': 'string',
-    'venue_semis': 'string',
-    'venue_finals': 'string',
-    'video_referee': 'bool', # video_referee
-    'promotion_teams': 'Int64', # pro_sys
-    'promotion_system': 'string', # pro_sys
-    'relegation_teams': 'Int64', # rel_sys
-    'relegation_system': 'string' # rel_sys
-}
-
-# 1901 Dictionary File Read
+# File Read
 print('Load League Data From Path: ' + dirLeagueName)
-footData = pd.read_csv(dataFootballLeagueName,header=0,names=footieHeader,dtype=footieTypes,index_col=False)
+#footData = pd.read_csv(dataFootballLeagueName,header=0,dtype=footieTypes,index_col=False)
+hockDF = pd.read_csv(dataHockLeagueName,header=0,dtype=const.leagueTypes,index_col=False)
 
 # standardisation
-footData['promotion_system'] = footData['promotion_system'].fillna('none')
-footData['relegation_system'] = footData['relegation_system'].fillna('none')
-footData['points_tiebreaker'] = footData['points_tiebreaker'].fillna('none')
-footData['victory'] = footData['victory'].fillna(0)
-footData['draw'] = footData['draw'].fillna(0)
-footData['loss'] = footData['loss'].fillna(0)
+#footData['victory'] = footData['victory'].fillna(0)
+#footData['draw'] = footData['draw'].fillna(0)
+#footData['loss'] = footData['loss'].fillna(0)
+#footData['points_tiebreaker'] = footData['points_tiebreaker'].fillna('none')
+#footData['promotion_system'] = footData['promotion_system'].fillna('none')
+#footData['relegation_system'] = footData['relegation_system'].fillna('none')
 
 # group data
-#footData['pro_sys'] = footData[['promotion_system', 'promotion_teams']].apply(tuple, axis=1)
-#footData['rel_sys'] = footData[['relegation_system', 'relegation_teams']].apply(tuple, axis=1)
 
 # expand
-footExpandedData = func.expandRange(footData,'format_start','format_end','format_range','format_year')
-footExpandedData = footExpandedData.sort_values(['format_year','level'],ascending=[True,True])
+#footExpandedData = func.expandRange(footData,'format_start','format_end','format_range','format_year')
+#footExpandedData = footExpandedData.sort_values(['format_year','level'],ascending=[True,True])
+hockExpDF = func.expandRange(hockDF,'format_start','format_end','format_range','format_year')
+hockExpDF = hockExpDF.sort_values(['format_year','competition_tier'],ascending=[True,True])
 
-# time format used weight
-#footData['format_weight'] = footData['format_end'] - footData['format_start'] + 1
+# aggregate
 
-# 'level'
-# 'name_base'
-# 'name_prefix'
-# 'name_suffix'
-# 'name_full'
-# 'name_short'
-# 'organiser'
-# 'organiser_name'
-#footData['entry_format'] = footData[['entry', 'clubs']].apply(tuple, axis=1)
-# 'stages'
-# 'qualification_spots'
-# 'qualification_byes_format'
-# 'tier_1_byes'
-# 'tier_1_byes_format'
-# 'tier_1_bye_entry_format'
-# 'tier_1_entry_format'
-# 'tier_1_intermediate'
-# 'tier_1_qualifying_third'
-# 'tier_1_qualifying_second'
-# 'tier_1_qualifying_first'
-# 'tier_1_qualifying_preliminary'
-# 'tier_2_byes'
-# 'tier_2_byes_format'
-# 'tier_2_bye_entry_format'
-# 'tier_2_entry_format'
-# 'tier_2_intermediate'
-# 'tier_2_qualifying_sixth'
-# 'tier_2_qualifying_fifth'
-# 'tier_2_qualifying_fourth'
-# 'tier_2_qualifying_third'
-# 'tier_2_qualifying_second'
-# 'tier_2_qualifying_first'
-# 'tier_2_qualifying_preliminary'
-# 'amateur_cup_byes'
-# 'amateur_cup_bye_entry_format'
-# 'non_association_byes'
-# 'non_association_bye_entry_format'
-# 'non_association_other_byes'
-# 'non_association_other_bye_entry_format'
-# 'invite_byes'
-# 'invite_bye_entry_format'
-# 'format_start'
-# 'format_end'
-# 'type'
-# 'quality'
-# 'gender'
-#footData['score_sys'] = footData[['victory', 'draw', 'loss', 'points_tiebreaker']].apply(tuple, axis=1)
-# 'match_system'
-# 'uneven_team_system'
-# 'draw_system'
-# 'extra_time'
-# 'previous_winner'
-# 'home_and_away'
-#footData['cup_venues'] = footData[['venue_system',  'replay_venue', 'add_replay_venue','venue_semis', 'venue_finals']].apply(tuple, axis=1)
-# 'video_referee'
-#footData['pro_sys'] = footData[['promotion_system', 'promotion_teams']].apply(tuple, axis=1)
-#footData['rel_sys'] = footData[['relegation_system', 'relegation_teams']].apply(tuple, axis=1)
+# Stat out likelyhood of league creation
+
+# Get first season for each competition and add as column
+startSeasonDF = hockExpDF.groupby('competition_name').agg(firstSeason =('format_year','min')).reset_index()
+startSeasonDi = dict(zip(startSeasonDF['competition_name'], startSeasonDF['firstSeason']))
+hockExpDF['firstSeason'] = hockExpDF['competition_name'].map(startSeasonDi)
+
+# Get last season for each competition and add as column
+lastSeasonDF = hockExpDF.groupby('competition_name').agg(lastSeason =('format_year','max')).reset_index()
+lastSeasonDi = dict(zip(lastSeasonDF['competition_name'], lastSeasonDF['lastSeason']))
+hockExpDF['lastSeason'] = hockExpDF['competition_name'].map(lastSeasonDi)
 
 
-footExpandedData.to_csv(os.path.join(resultsInterDirName,'expanded.csv'),index=False)
+# Filter if row season date equals first season date column
+hockSSDF = hockExpDF[hockExpDF['firstSeason'] == hockExpDF['format_year']].drop_duplicates(subset=['format_year','competition_name'])
+# Filter if row season date equals last season date column
+hockLSDF = hockExpDF[hockExpDF['lastSeason'] == hockExpDF['format_year']].drop_duplicates(subset=['format_year','competition_name'])
 
-orgFootData = footExpandedData.groupby('organiser_name')
+# Count these creations by season
+compCreationDF = hockSSDF['format_year'].value_counts().reset_index()
+compCreationDi = dict(zip(compCreationDF['format_year'], compCreationDF['count']))
 
-for name, group in orgFootData:
-    fileName = '{filePrefix}_{grouping}_{name}.csv'.format(filePrefix='eng',grouping='foot',name=name)
-    dirName = os.path.join(resultsInterDirName,'{grouping}_{name}'.format(grouping='foot',name=name))
-    os.makedirs(dirName, exist_ok=True)
+# Count these dissolution by season
+compDissolutionDF = hockLSDF['format_year'].value_counts().reset_index()
+compDissolutionDF['format_year'] = compDissolutionDF['format_year']+1
+compDissolutionDi = dict(zip(compDissolutionDF['format_year'], compDissolutionDF['count']))
 
-    print('Writing {name}'.format(name=fileName))
-    group.to_csv(os.path.join(dirName,fileName), index=False)
+# Count reported competitions by season
+yearDF = hockExpDF.drop_duplicates(subset=['format_year','competition_name']).groupby('format_year').agg(competitions =('competition_name','count')).reset_index()
+yearDF.index = yearDF['format_year']
+yearDF = yearDF.reindex(np.arange(yearDF['format_year'].min(), yearDF['format_year'].max() + 1)).fillna(0)
+yearDF['competitions'] = yearDF['competitions'].astype(int)
+yearDF = yearDF.drop(['format_year'],axis=1).reset_index()
+
+# Add In Creation Events
+yearDF['competitionsCreated'] = yearDF['format_year'].map(compCreationDi)
+yearDF['competitionsCreated'] = yearDF['competitionsCreated'].fillna(0).astype(int)
+yearDF['competitionsFolded'] = yearDF['format_year'].map(compDissolutionDi)
+yearDF['competitionsFolded'] = yearDF['competitionsFolded'].fillna(0).astype(int)
+
+hockSSDF[["format_year", "competition_name", "competition_type"]].to_csv(os.path.join(resultsInterDirName,'hockey_comp_start.csv'),index=False)
+yearDF.to_csv(os.path.join(resultsInterDirName,'hockey_expanded.csv'),index=False)
+#
+#
+#
+#for name, group in orgFootData:
+#    fileName = '{filePrefix}_{grouping}_{name}.csv'.format(filePrefix='eng',grouping='foot',name=name)
+#    dirName = os.path.join(resultsInterDirName,'{grouping}_{name}'.format(grouping='foot',name=name))
+#    os.makedirs(dirName, exist_ok=True)
+#
+#    print('Writing {name}'.format(name=fileName))
+#    group.to_csv(os.path.join(dirName,fileName), index=False)
